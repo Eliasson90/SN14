aa414e0be471ae1e45f3ad0ecbe628092cedb5f5
Robin Eliasson <r.eliasson90@gmail.com>
2015-01-07 14:54:57 +0100

    labb 2 A uppgift

---
+++ labb2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //rader
+            for (int row = 0; row < 25; row++)
+            {
+                if (row % 2 == 1)
+                {
+                    Console.Write(" ");
+                }
+
+                //byta färger
+                switch (row % 3)
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    
+                }
+
+               
+
+                // kolumner
+                for (int col = 0; col < 39; col++)
+                {
+
+                    Console.Write("* ");
+
+                   
+                }
+                //reseta färgen
+                Console.ResetColor();
+                //skriv ut press to continue
+                Console.WriteLine();
+                
+            }
+
+            
+        }
+        
+    }
+}
+
