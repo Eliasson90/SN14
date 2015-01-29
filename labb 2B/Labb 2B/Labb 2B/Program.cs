4ab155e9b0e5ae3df661836e1378cc82cc37c476
Robin Eliasson <r.eliasson90@gmail.com>
2015-01-29 14:32:51 +0100

    labb 2b påbörjad

---
+++ labb 2B/Labb 2B/Labb 2B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                while(true)
+                {
+
+                }
+
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                Console.WriteLine("Tryck på valfri tagent för att börja om - ESC avslutar.");
+                Console.ResetColor();
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        public byte ReadOddByte()
+        {
+
+        }
+
+        public void RenderTriangle(byte cols)
+        {
+
+        }
+    }
+}
