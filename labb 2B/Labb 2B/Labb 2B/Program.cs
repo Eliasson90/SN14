e79f7624cca09c32dbccb8a1a5e4d201bd405d96
Robin Eliasson <r.eliasson90@gmail.com>
2015-02-03 18:20:23 +0100

    laggt till kod

--- labb 2B/Labb 2B/Labb 2B/Program.cs
+++ labb 2B/Labb 2B/Labb 2B/Program.cs
@@ -8,14 +8,25 @@ namespace Labb_2B
 {
     class Program
     {
+        const byte MinBas = 1;
+        const byte MaxBas = 79;
+        const string Title = "Ange ett udda tal asterisker , max 79 i trianglens bas: ";
+
         static void Main(string[] args)
         {
             do
             {
-                while(true)
-                {
+                Console.Clear();
 
-                }
+                //anropa inläsning av tal
+
+                byte amount = ReadOddByte();
+
+                //anropa DrawTriangle för visning av triangle
+
+                RenderTriangle(amount);
+
+                //Avsluta med esc 
 
                 Console.BackgroundColor = ConsoleColor.DarkBlue;
                 Console.ForegroundColor = ConsoleColor.White;
@@ -26,14 +37,55 @@ namespace Labb_2B
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
 
-        public byte ReadOddByte()
+        public static byte ReadOddByte()
         {
+            byte number = 0;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write(Title);
+                    number = byte.Parse(Console.ReadLine());
+
+                    if (number % 2 == 0 || number < MinBas && number > MaxBas)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Fel! det inmartade värdet är inte ett udda tal mellan {0} och {1}!. ", MinBas, MaxBas);
+                        Console.ResetColor();
+                        continue;
+                    }
+                    else
+                    {
+                        return number;
+                    }
+
+                }
+                catch
+                {
+                    throw new ArgumentException();
+                }
+            }
 
         }
 
-        public void RenderTriangle(byte cols)
+        public static void RenderTriangle(byte cols)
         {
+            //rita ut trianglen
 
+            for (int i = 0; i < cols; i += 2)
+            {
+                for (int j = cols - i; j >= 0; j -= 2)
+                {
+                    Console.Write("  ");
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
